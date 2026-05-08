ac458891df9c622d25b9564885bf4a1286370c0a
Julsanchezc <julsanchezc@unal.edu.co>
2026-05-08 15:46:24 -0500

    feat: add right rotation method to ArbolAVL class for balancing

--- app/structures/ArbolAVL.java
+++ app/structures/ArbolAVL.java
@@ -24,6 +24,26 @@ public class ArbolAVL {
             n.setAltura(1 + Math.max(altura(n.getIzquierda()), altura(n.getDerecha())));
     }
 
+    // =========================================================
+    // ROTACION SIMPLE DERECHA
+    // Caso: subarbol izquierdo demasiado alto y su hijo izq. es mas alto
+    // =========================================================
+    private NodoAVL rotarDerecha(NodoAVL n) {
+        NodoAVL m = n.getIzquierda();
+        if (m.getDerecha() != null) m.getDerecha().setPadre(n);
+        m.setPadre(n.getPadre());
+        if      (n.getPadre() == null)                          raiz = m;
+        else if (n == n.getPadre().getIzquierda()) n.getPadre().setIzquierda(m);
+        else                                        n.getPadre().setDerecha(m);
+        n.setIzquierda(m.getDerecha());
+        m.setDerecha(n);
+        n.setPadre(m);
+        ajustarAltura(n);
+        ajustarAltura(m);
+        return m;
+    }
+
+
 
 
 }
